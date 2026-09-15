9db99d206a3e970140a61e0af652901248a6a3a0
Felipe-alves518 <brisket.bucket@gmail.com>
2026-09-15 10:30:46 -0300

    post request handler sends json to database

--- test_database2/backend/src/main.rs
+++ test_database2/backend/src/main.rs
@@ -1,9 +1,33 @@
-use axum::{routing::get, Router};
+use axum::{routing::post, Router};
+use axum::extract::Json;
+use serde::{Deserialize};
+
+
+#[derive(Deserialize)]
+struct CreateUser {
+    nome: String,
+    rf: String,
+    setor: String, 
+}
+
+async fn json(Json(payload): Json<CreateUser>) {
+    let connection = sqlite::open("teste.db").unwrap();
+    let query = format!("INSERT INTO teste (nome,rf,setor) VALUES ('{}','{}','{}')",payload.nome,payload.rf,payload.setor);
+    connection.execute(
+"CREATE TABLE IF NOT EXISTS teste (
+            nome TEXT,
+            rf   TEXT,
+            setor TEXT
+            );").unwrap();
+    connection.execute(query).unwrap();
+}
 
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async {"hello, World"}));
+
+    let app = Router::new()
+                      .route("/", post(json));
 
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000").await.unwrap();
